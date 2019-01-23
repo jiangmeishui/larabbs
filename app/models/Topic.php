68b5f44529db24c0fefd42c8ef1c7d27070b559c
Jiang <237132868@qq.com>
2019-01-23 02:52:03 +0000

    话题排序

--- app/models/Topic.php
+++ app/models/Topic.php
@@ -13,4 +13,28 @@ class Topic extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
+
+    // 使用本地作用域,作集合条件约束,返回一个查询构造器,需要在方法前加score前缀,调用时
+    // 不用加
+    public function scopeWithOrder($query, $order) {
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+            default:
+                $query->recentReplied();
+                break;
+        }
+        // 由于有关联模型,预防N+1问题
+        return $query->with('user', 'category');
+    }
+
+    public function scopeRecent($query) {
+        // 按创建时间排序
+        return $query->orderBy('created_at', 'desc');
+    }
+
+    public function scopeRecentReplied($query) {
+        return  $query->orderBy('updated_at', 'desc');
+    }
 }
