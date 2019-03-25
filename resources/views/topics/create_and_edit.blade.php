d740cafe9ec7f59febd6553a05f3101f703a1c73
vagrant <vagrant@homestead>
2019-03-25 10:07:31 +0000

    用户可以上传话题图片

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -2,7 +2,6 @@
 @section('styles')
     <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
 @stop
-
 @section('scripts')
     <script type="text/javascript" src="{{ asset('js/module.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>
@@ -13,6 +12,16 @@
         $(document).ready(function() {
             var editor = new Simditor({
                 textarea: $('#editor'),
+                upload: {
+                    url: '{{ route('topics.upload_image') }}',
+                    params: {
+                        _token: '{{ csrf_token() }}'
+                    },
+                    fileKey: 'upload_file',
+                    connectionCount: 3,
+                    leaveConfirm: '文件上传中，关闭此页面将取消上传。'
+                },
+                pasteImage: true,
             });
         });
     </script>
