a26360b058a1ea165419b0c9e3774dd9207fe77e
vagrant <vagrant@homestead>
2019-03-26 12:56:41 +0000

    消息通知有新回复

--- app/Http/Requests/ReplyRequest.php
+++ app/Http/Requests/ReplyRequest.php
@@ -7,7 +7,7 @@ class ReplyRequest extends Request
     public function rules()
     {
         return [
-            'content' => 'required|min:2',
+            'contents' => 'required|min:2',
         ];
     }
 
